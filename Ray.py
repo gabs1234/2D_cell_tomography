cbfcd34ecb367c5b1b51780b663cecac78be9622
Gabriel Lefloch <gabriel.lefloch@cea.fr>
2022-01-09 20:25:38 +0100

    vertical and horizontal lines work. But not lines with other directions

--- Ray.py
+++ Ray.py
@@ -1,4 +1,43 @@
+from Cell import Cell
+import numpy as np
+
 class Ray(object):
 	def __init__(self, origin, dir):
 		self.origin = np.array(origin)
 		self.direction = np.array(dir)
+		
+		self.sign = np.where(self.direction < 0, [1]*len(self.direction), [0]*len(self.direction))
+
+		# Avoid printing error when infinity encoutered
+		with np.errstate(divide='ignore'):
+			self.invDirection = np.divide(1.0, self.direction)
+
+		# Signal along which axis the line is constant
+		self.constantAxis = (self.direction == 0)
+
+	def intersects(self, cell):
+		bounds = cell.bounds
+
+		for i, constant in enumerate(self.constantAxis):
+			if( constant ):
+				if( not (bounds[i][0] <= self.origin[i] <= bounds[i][1]) ):
+					return False
+
+		if( self.constantAxis[0] ):
+			tmin = -np.inf
+			tmax = +np.inf
+		else:
+			tmin = (bounds[:, self.sign[0]][0] - self.origin[0]) * self.invDirection[0]
+			tmax = (bounds[:, 1 - self.sign[0]][0] - self.origin[0]) * self.invDirection[0]
+
+		if( self.constantAxis[1] ):
+			tymin = -np.inf
+			tymax = +np.inf
+		else:
+			tymin = (bounds[:, self.sign[1]][1] - self.origin[1]) * self.invDirection[1]
+			tymax = (bounds[:, 1 - self.sign[1]][1] - self.origin[1]) * self.invDirection[1]
+
+		if( (tmin >= tymax) or (tymin >= tmax) ):
+			return False
+		
+		return True
--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import numpy as np
 from math import *
+from Ray import Ray
+from Cell import Cell
 from Grid import Grid
 
-## PARAMETERS ##
-# Cells
-x_len = 1
-y_len = 1
-nb_x_cells = 3
-nb_y_cells = nb_x_cells
-
-nb_cells = nb_x_cells*nb_y_cells
-
-len_x_cell = x_len/nb_x_cells
-len_y_cell = y_len/nb_y_cells
-diag_cell = sqrt(len_x_cell**2 + len_y_cell**2)
-
-# Rays
-nb_rays = nb_x_cells + nb_y_cells + 2*(nb_x_cells - 1 + nb_y_cells)
-
-# Init G matrix
-N, K = nb_rays, nb_x_cells*nb_y_cells
-G = np.zeros([N, K])
-
 grid = Grid(3, 3, 1, 1)
+
+ray = Ray((0, 0), (-1, -1))
+
+inter = grid._findIntersectingPoints(ray)
+
+for cell in inter:
+	cell.disp()
+
+print("grid:")
 grid.disp()
